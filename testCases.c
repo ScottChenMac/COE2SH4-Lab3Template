ce60ef310d85383b417530e23df719a37f5118a5
keithraiter <67427459+keithraiter@users.noreply.github.com>
2023-10-10 13:33:00 -0400

    correction in test cases

--- testCases.c
+++ testCases.c
@@ -176,114 +176,15 @@
 		free(str_actual);
 	}
 
+
+// This section is commented out because Q2 contains a buggy code to be debugged.
+
+// Uncomment this section only after you are done with Q1.
 /*	
 
-    
 //===========================================================
-//=================Question 2================================
-	void TestQ2_find_notThere(CuTest *tc) {
-
-		char inputFile[] =  "students.txt";
-		int size;
-		//create list using the input file
-		student **Clist = create_class_list(inputFile,&size);
-
-		int expected = -1;
-		int actual = find(0011,Clist,size);
-
-		CuAssertIntEquals(tc, expected, actual);
-
-	}
-
-	void TestQ2_find_There(CuTest *tc) {
-
-		char inputFile[] =  "students.txt";
-		int size;
-		//create list using the input file
-		student **Clist = create_class_list(inputFile,&size);
-
-		int expected = 4;
-		int actual = find(9000,Clist,size);
-
-		CuAssertIntEquals(tc, expected, actual);
-
-	}
-
-	void TestQ2_inputGradesAndCalculateFinal(CuTest *tc) {
-
-		
-
-		char inputFile[] =  "students.txt";
-		char gradesFile[] =  "grades.txt";
-		int size;
-		//create list using the input file
-		student **Clist = create_class_list(inputFile,&size);
-		//input grades using the grades input file
-		input_grades(gradesFile,Clist,size);
-		//calculate final grades
-		compute_final_course_grades(Clist,size);
-		//Output final course marks
-		output_final_course_grades("outputGrades.txt",Clist,size);
-
-		//now we open the golden file with the written file and compare line by line
-
-		char actualFile[] =  "outputGrades.txt";
-		char goldenFile[] =  "output.txt";
-
-		char actual[1000], expected[1000];
-		int actualID,expectedID;
-		double actualFinalGrade,expectedFinalGrade;
-		FILE *actualPtr, *expectedPtr;
-		if ((actualPtr = fopen(actualFile, "r")) == NULL || (expectedPtr = fopen(goldenFile, "r")) == NULL) {
-			printf("Error! opening file");
-			// Program exits if file pointer returns NULL.
-			exit(1);
-		}
-
-		int actualNumStudents,expectedNumStudents;
-
-
-		fscanf(actualPtr,"%d",&actualNumStudents);
-		
-		fscanf(expectedPtr,"%d",&expectedNumStudents);
-		
-		CuAssertIntEquals(tc, expectedNumStudents, actualNumStudents);
-
-		int i;
-		for(i=0;i<expectedNumStudents;i++){
-		//while (actualPtr != NULL && expectedPtr!=NULL){
-			// reads text until newline is encountered
-			//fscanf(actualPtr, "%[^\n]", actual);
-			//printf("%s ", actual);
-			//fscanf(expectedPtr, "%[^\n]", expected);
-			//printf("%s\n", expected);
-
-			//CuAssertStrEquals(tc, expected, actual);
-
-		
-			fscanf(actualPtr,"%d",&actualID);
-			
-			fscanf(expectedPtr,"%d",&expectedID);
-			
-			CuAssertIntEquals(tc, expectedID, actualID);
-
-			fscanf(actualPtr,"%lf",&actualFinalGrade);
-			fscanf(expectedPtr,"%lf",&expectedFinalGrade);
-			CuAssertDblEquals(tc, expectedFinalGrade, actualFinalGrade,0.000001);
-		}
-		if ((actualPtr == NULL && expectedPtr!=NULL)|| (actualPtr != NULL && expectedPtr==NULL)){
-
-			printf("files are not of equal size");
-			// Program exits if file pointer returns NULL.
-			exit(1);
-
-		}
-
-	}
-
-//===========================================================
-//=================Question 3================================  
-	void TestQ3_readandSort1(CuTest *tc) {
+//=================Question 2================================  
+	void TestQ2_readandSort1(CuTest *tc) {
 
 		char inputFile[] =  "wordlist.txt";
 		int size;
@@ -299,7 +200,7 @@
 
 	}
    
-	void TestQ3_readandSort2(CuTest *tc) {
+	void TestQ2_readandSort2(CuTest *tc) {
 
 		char inputFile[] =  "wordlist.txt";
 		int size;
@@ -344,8 +245,8 @@
 		SUITE_ADD_TEST(suite, TestQ1_strcat_FirstEmpty);
 		SUITE_ADD_TEST(suite, TestQ1_strcat_SecondEmpty);
 
-//		SUITE_ADD_TEST(suite, TestQ3_readandSort1);
-//		SUITE_ADD_TEST(suite, TestQ3_readandSort2);
+//		SUITE_ADD_TEST(suite, TestQ2_readandSort1);
+//		SUITE_ADD_TEST(suite, TestQ2_readandSort2);
         
        
 
